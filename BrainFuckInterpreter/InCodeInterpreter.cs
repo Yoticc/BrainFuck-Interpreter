f3919027634d0ea07b14a0591cb475fea423b8af
Yotic <yoticcitoy@gmail.com>
2022-12-14 19:38:40 +0300

    Add project files.

---
+++ BrainFuckInterpreter/InCodeInterpreter.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainFuckInterpreter;
+public class InCodeInterpreter
+{
+    private int pos = 0;
+    private int allocSize;
+    private MemoryStream stream;
+    public InCodeInterpreter(int allocSize)
+    {
+        this.allocSize = allocSize;
+        stream = new MemoryStream(allocSize);
+    }
+    public InCodeInterpreter(int allocSize, MemoryStream memoryStream, int pos = 0)
+    {
+        this.allocSize = allocSize;
+        this.pos = pos;
+        stream = memoryStream;
+    }
+    public Stream GS()
+    {
+        stream.Position = 0;
+        return stream;
+    }
+    public byte[] GB()
+    {
+        stream.Position = 0;
+        byte[] bytes = new byte[allocSize];
+        stream.Read(bytes, 0, bytes.Length);
+        return bytes;
+    }
+    public InCodeInterpreter p
+    {
+        get
+        {
+            Console.Write(stream.ReadByte());
+            stream.Position = pos;
+            return this;
+        }
+    }
+    public InCodeInterpreter w
+    {
+        get
+        {
+            stream.WriteByte((byte)(Console.ReadLine() ?? "")[0]);
+            stream.Position = pos;
+            return this;
+        }
+    }
+    public static InCodeInterpreter operator /(InCodeInterpreter bf, short rem)
+    {
+        byte newByte = (byte)(bf.stream.ReadByte() - rem);
+        bf.stream.Position = bf.pos;
+        bf.stream.WriteByte(newByte);
+        bf.stream.Position = bf.pos;
+        return new(bf.allocSize, bf.stream, bf.pos);
+    }
+    public static InCodeInterpreter operator *(InCodeInterpreter bf, short add)
+    {
+        byte newByte = (byte)(bf.stream.ReadByte() + add);
+        bf.stream.Position = bf.pos;
+        bf.stream.WriteByte(newByte);
+        bf.stream.Position = bf.pos;
+        return new(bf.allocSize, bf.stream, bf.pos);
+    }
+    public static InCodeInterpreter operator %(InCodeInterpreter bf, int length)
+    {
+        bf.stream.Position = bf.pos = bf.pos + length;
+        return new(bf.allocSize, bf.stream, bf.pos);
+    }
+}
---
+++ BrainFuckInterpreter/StringInterpreter.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrainFuckInterpreter;
+public class StringInterpreter
+{
+    public static byte[] Execute(string code, int allocSize = 30000, int pos = 0)
+    {
+        var bytes = new byte[allocSize];
+        Dictionary<int, int> point = new Dictionary<int, int> { { -9, 0 }, { -2, 0 }, { -1, 0 }, { 0, 0 } };
+        Dictionary<byte, Action> types = new(){
+            { 62, new(() => pos++) }, { 60, new(() => pos-- ) },
+            { 43, new(() => bytes[pos]++ ) }, { 45, new(() => bytes[pos]--) },
+            { 46, new(() => Console.Write((char)bytes[pos])) }, { 44, new(() => bytes[pos] = (byte)(Console.ReadLine()??" ")[0]) },
+            { 91, new(() => {
+                if (bytes[pos] == 0)
+                    do point[-1] += (code[++point[point[-2]] - 1] == 91 ? 1 : (code[point[point[-2]] - 1] == 93 ? -1 : 0));
+                    while (code[point[point[-2]] - 1] != 93 || point[-1] != 0);
+                else point[++point[-2]] = point[point[-2] - 1] + 1;
+                point[point[-2]]--;
+            })},
+            { 93, new(() => point[-9] = (bytes[pos] != 0 ? new(() => point[point[-2]] = point[point[-2] - 1] + 1) : new Action(() => point[-2]--))() is int ? point[point[-2]]-- : point[point[-2]]--)}
+        };
+        while (point[point[-2]] < code.Length)
+            point[-9] = types[(byte)code[point[point[-2]]]]() is int ? point[point[-2]]++ : point[point[-2]]++;
+        return bytes;
+    }
+}
