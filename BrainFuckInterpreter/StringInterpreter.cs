9848e80784f5b4f349e0a26a3ddf543d495e3dae
Yotic <yoticcitoy@gmail.com>
2024-07-09 08:02:16 +0300

    Even less

--- BrainFuckInterpreter/StringInterpreter.cs
+++ BrainFuckInterpreter/StringInterpreter.cs
@@ -1,25 +1,17 @@
 ﻿namespace BrainFuckInterpreter;
 public class StringInterpreter
 {
-    public static byte[] Execute(string code, int allocSize = 30000, int pos = 0)
+    public static char[] Execute(string code, int allocSize)
     {
-        var bytes = new byte[allocSize];
-        var point = new Dictionary<int, int> { { -9, 0 }, { -2, 0 }, { -1, 0 }, { 0, 0 } };
-        Dictionary<byte, Action> types = new(){
-            { 62, new(() => pos++) }, { 60, new(() => pos-- ) },
-            { 43, new(() => bytes[pos]++ ) }, { 45, new(() => bytes[pos]--) },
-            { 46, new(() => Console.Write((char)bytes[pos])) }, { 44, new(() => bytes[pos] = (byte)(Console.ReadLine()??" ")[0]) },
-            { 91, new(() => {
-                if (bytes[pos] == 0)
-                    do point[-1] += (code[++point[point[-2]] - 1] == 91 ? 1 : (code[point[point[-2]] - 1] == 93 ? -1 : 0));
-                    while (code[point[point[-2]] - 1] != 93 || point[-1] != 0);
-                else point[++point[-2]] = point[point[-2] - 1] + 1;
-                point[point[-2]]--;
-            })},
-            { 93, new(() => point[-9] = (bytes[pos] != 0 ? new(() => point[point[-2]] = point[point[-2] - 1] + 1) : new Action(() => point[-2]--))() is int ? point[point[-2]]-- : point[point[-2]]--)}
-        };
-        while (point[point[-2]] < code.Length)
-            point[-9] = types[(byte)code[point[point[-2]]]]() is int ? point[point[-2]]++ : point[point[-2]]++;
-        return bytes;
+        var(z,v,o,n)=(new char[allocSize],0,new Dictionary<int,int>{{-9,0},{-2,0},{-1,0},{0,0}},code);
+        while(o[o[-2]]<n.Length)o[-9]=((Action[])[()=>v++,()=>v--,()=>z[v]++,()=>z[v]--,()=>Console.Write(z[v]),()=>z[v]=Console.ReadLine()[0],()=>{
+            if(z[v]==0)
+                do o[-1]+=n[++o[o[-2]]-1]==91?1:(n[o[o[-2]]-1]==93?-1:0);
+                while(n[o[o[-2]]-1]!=93||o[-1]!=0);
+            else o[++o[-2]]=o[o[-2]-1]+1;o[o[-2]]--;
+        },()=>o[-9]=(z[v]!=0?(Action)(()=>o[o[-2]]=o[o[-2]-1]+1):(()=>o[-2]--))()is int?0:o[o[-2]]--])[((List<int>)[62,60,43,45,46,44,91,93]).IndexOf((byte)n[o[o[-2]]])]()is int?0:o[o[-2]]++;
+        return z;
     }
 }
+
+
